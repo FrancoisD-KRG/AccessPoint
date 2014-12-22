f84a15775d7d9904838d99e434c19709b56228d4
FrancoisD-KRG <krg.francois.d@gmail.com>
2014-12-22 15:14:43 +0100

    Socket created

--- src/main/java/com/access/HelloServlet.java
+++ src/main/java/com/access/HelloServlet.java
@@ -2,7 +2,7 @@ package main.java.com.access; /**
  * Created by francoisdziedzinl on 08/12/2014.
  */
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,19 +19,29 @@ public class HelloServlet extends HttpServlet {
 		out.println("The paramter q was \"" + q + "\".");
 		out.println("</body>");
 		out.println("</html>");
-
-
 	}
 
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String field = request.getParameter("command1");
-		String field2 = request.getParameter("field");
+		String command = request.getParameter("command");
 		PrintWriter out = response.getWriter();
 
 		out.println("<html>");
 		out.println("<body>");
-		out.println("You entered \"" + "field: " + field + " and field2: " + field2 + "\" into the text box.");
+		out.println("You entered the command: " + command);
 		out.println("</body>");
 		out.println("</html>");
+
+		Socket socket;
+		PrintStream output;
+
+		try {
+			socket = new Socket("localhost", 9090);
+			output = new PrintStream(socket.getOutputStream());
+			output.print(command);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
 }
--- src/main/java/com/access/HelloWorld.java
+++ src/main/java/com/access/HelloWorld.java
@@ -2,15 +2,11 @@ package main.java.com.access; /**
  * Created by francoisdziedzinl on 08/12/2014.
  */
 
-import main.java.com.access.rest.Hello;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
 
 public class HelloWorld
 {
@@ -20,15 +16,10 @@ public class HelloWorld
 		Server server = new Server(8080);
 		ServletHandler servlethandler = new ServletHandler();
 		servlethandler.addServletWithMapping(HelloServlet.class, "/*");
-/*
-		ServletHolder sh = new ServletHolder(ServletContainer.class);
-		sh.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
-		sh.setInitParameter("com.sun.jersey.config.property.packages", "rest");//Set the package where the services reside
-		sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
-*/
+
 		ResourceHandler resourcehandler = new ResourceHandler();
 		resourcehandler.setDirectoriesListed(true);
-		resourcehandler.setResourceBase("out/production/Test3/");
+		resourcehandler.setResourceBase("out/production/AccessPoint/");
 		resourcehandler.setWelcomeFiles(new String[]{"main/resources/webapp/index.html"});
 
 		HandlerList handlers = new HandlerList();
@@ -37,12 +28,6 @@ public class HelloWorld
 
 		server.start();
 		server.join();
-		/*
 
-		ResourceConfig resourceConfig = new ResourceConfig();
-		resourceConfig.packages(Hello.class.getPackage().getName());
-		ServletContainer servletContainer = new ServletContainer(resourceConfig);
-		ServletHolder sh = new ServletHolder(servletContainer);
-*/
 	}
 }
